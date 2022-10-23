ce943bbcd958be298e67671cbd90c7a8ffbb935e
Alfred Ang <angch@tertiaryinfotech.com>
2022-10-23 23:41:28 +0800

    first commit

--- exercises/module5_3_gensim_word2vec_load.py
+++
@@ -1,9 +0,0 @@
-# Module 5: Word Embedding
-# Word2Vec Load Model
-# Author: Dr. Alfred
-
-from gensim.models import Word2Vec
-from nltk.corpus import gutenberg
-
-embedding = Word2Vec.load('gutenberg_model')
-print(embedding.most_similar('man', topn=5))
